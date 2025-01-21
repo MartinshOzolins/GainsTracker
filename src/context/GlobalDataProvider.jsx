06b48cd9c0e33e231b26275fb7d2469eac4e0092
Martins Ozolins <ozolins.martinsh@gmail.com>
2025-01-21 10:15:47 +0000

    Update imports

--- src/context/GlobalDataProvider.jsx
+++ src/context/GlobalDataProvider.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { GlobalDataContext } from "../context/globalDataContext.js";
+import { GlobalDataContext } from "./globalDataContext.js";
 
 export default function GlobalDataProvider({ children }) {
   const [currentUser, setCurrentUser] = useState(undefined);
